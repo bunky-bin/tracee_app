f07fc3183bd7b72a3c3404efe1529ff3c72bcc89
Simone Piccolo <simonepiccolo1987@gmail.com>
2022-05-28 16:03:55 +0100

    links through the pages settled, base index done and same for show

---
+++ app/controllers/helpers_controller.rb
@@ -0,0 +1,11 @@
+class HelpersController < ApplicationController
+  def index
+    @helpers = User.all
+
+  end
+  def show
+    @helper = User.find(params[:id])
+    @request = Request.new
+
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "cleaning database"
+User.destroy_all
+
+puts "start seeds"
+
+userone = User.create!(first_name: "T", last_name: "Lave", email: "lavellesine@gmail.com", password: "123456",
+                        availability: true)
+usertwo = User.create!(first_name: "Mark", last_name: "Wales", email: "testoone@gmail.com", password: "123456",
+                       availability: true)
+usertree = User.create!(first_name: "Lucas", last_name: "La croux", email: "tryemail@gmail.com", password: "123456",
+                        availability: false)
+puts "users created"
