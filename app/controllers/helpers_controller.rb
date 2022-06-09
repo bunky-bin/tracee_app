0172f7dfa37743dff5d151d13a83d75905e6dc03
Anna G <85934747+bunky-bin@users.noreply.github.com>
2022-06-09 19:31:16 +0100

    mapbox working

--- app/controllers/helpers_controller.rb
+++ app/controllers/helpers_controller.rb
@@ -1,10 +1,21 @@
 class HelpersController < ApplicationController
   def index
-    @helpers = User.all
+    @helpers = User.geocoded
+
+    @users = User.all
+
+    @markers = @users.geocoded.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { user: user })
+      }
+    end
   end
 
   def show
     @helper = User.find(params[:id])
     # @request = Request.new
   end
+
 end
