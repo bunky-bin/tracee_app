4783b40fad2b4c030f313628bbc07ac8dd4cd3a3
La-Velle <lavellesine@gmail.com>
2022-06-11 11:56:44 +0100

    images-and-note

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   has_many :received_requests, class_name: "Request", foreign_key: "helper_id"
   has_many :requests, foreign_key: "commuter_id"
   has_many :reviews
-  belongs_to :photo
+  has_one_attached :photo
   attr_reader :helper_id, :commuter_id
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -43,58 +43,185 @@
 
 require "open-uri"
 
-file = URI.open('https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg')
-article = Article.new(title: 'NES', body: "A great console")
-article.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
-article.save
+file = URI.open('https://t4.ftcdn.net/jpg/02/61/68/85/240_F_261688519_Dapj0DMehyGFRInEK3eGFzYBkZ5g27D6.jpg')
+# article = Article.new(title: 'NES', body: "A great console") sam-smith
+
 
 sam_smith = User.new(
   first_name: "Sam",
   last_name: "Smith",
   password: "123456",
   email: "samsmith@gmail.com",
-
+  bio: "Love singing",
+  availability: false
 )
-sam_smith.save
-# password: "password",
-#     first_name: Faker::Name.first_name,
-#     last_name: Faker::Name.last_name,
-#     email: Faker::Internet.email,
+sam_smith.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
+sam_smith.save!
 #     location: Faker::Address.full_address,
 #     latitude: Faker::Address.latitude,
 #     longitude: Faker::Address.longitude,
-#     availability: false
-#   )
+# TO BE ADDED AFTER GEOCODE ABSORBED INTO MASTER
 
-# Alex John = User.new(name: "Alex_John")
-# Alex_John.save
+file = URI.open('https://images.freeimages.com/images/large-previews/944/portrait-1623895.jpg')
+# article = Article.new(title: 'NES', body: "A great console") sam-smith
+#
 
-# Charlie Pink = User.new(name: "Charlie_Pink")
-# Charlie_Pink.save
+alex_john = User.new(
+  first_name: "Alex",
+  last_name: "John",
+  password: "123456",
+  email: "alexjohn@gmail.com",
+  bio: "Always out walking the dogs",
+  availability: false
+)
+alex_john.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
+alex_john.save!
 
-# Rory North = User.new(name: "Rory_North")
-# Rory_North.save
+file = URI.open('https://images.freeimages.com/images/large-previews/f3f/portrait-1438907.jpg')
+# article = Article.new(title: 'NES', body: "A great console") sam-smith
+#
 
-# Morgan Gray = User.new(name: "Morgan_Gray")
-# Morgan_Gray.save
+charlie_pink = User.new(
+  first_name: "Charlie",
+  last_name: "Pink",
+  password: "123456",
+  email: "charliepink@gmail.com",
+  bio: "work in the local coffee shop",
+  availability: false
+)
+charlie_pink.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
+charlie_pink.save!
 
-# Quinn Lee = User.new(name: "Quinn_Lee")
-# Quinn_Lee.save
+file = URI.open('https://pixy.org/src/11/thumbs350/118458.jpg')
+# article = Article.new(title: 'NES', body: "A great console") sam-smith
+#
 
-# Robin Bird = User.new(name: "Robin_Bird")
-# Robin_Bird.save
+rory_north = User.new(
+  first_name: "Rory",
+  last_name: "North",
+  password: "123456",
+  email: "rorynorth@gmail.com",
+  bio: "Local cabbie 24/7",
+  availability: false
+)
+rory_north.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
+rory_north.save!
 
-# Rowann Bush = User.new(name: "Rowann_Bush")
-# Rowann_Bush.save
+file = URI.open('https://i.huffpost.com/gen/1364522/thumbs/o-MICHAEL-PHARAOH-900.jpg?6')
+# article = Article.new(title: 'NES', body: "A great console") sam-smith
+#
 
-# Kelly Britton = User.new(name: "Kelly_Britton")
-# Kelly_Britton.save
+morgan_gray = User.new(
+  first_name: "Morgan",
+  last_name: "Gray",
+  password: "123456",
+  email: "morgangray@gmail.com",
+  bio: "Lived in this neighbourhood all my life I know everyone!",
+  availability: false
+)
+morgan_gray.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
+morgan_gray.save!
 
-# Cleo Smart = User.new(name: "Cleo_Smart")
-# Cleo_Smart.save
+file = URI.open('https://jooinn.com/images/a-beautiful-young-business-woman-14.jpg')
+# article = Article.new(title: 'NES', body: "A great console") sam-smith
+#
 
-# Jessie Johnson = User.new(name: "Jessie_Johnson")
-# Jessie_Johnson.save
+quinn_lee = User.new(
+  first_name: "Quinn",
+  last_name: "Lee",
+  password: "123456",
+  email: "quinnlee@gmail.com",
+  bio: "Baking cakes or out walking",
+  availability: false
+)
+quinn_lee.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
+quinn_lee.save!
 
-# Jackie Haruki = User.new(name: "Jackie_Haruki")
-# Jackie_Haruki.save
+file = URI.open('https://live.staticflickr.com/5204/5248278252_7bed0d3ebe_b.jpg')
+# article = Article.new(title: 'NES', body: "A great console") sam-smith
+#
+
+robin_bird = User.new(
+  first_name: "Robin",
+  last_name: "Bird",
+  password: "123456",
+  email: "robinbird@gmail.com",
+  bio: "Knitting or bird watching otherwise out walking",
+  availability: false
+)
+robin_bird.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
+robin_bird.save!
+
+file = URI.open('https://i.pinimg.com/originals/44/f6/22/44f622710b29893e0a310608443560da.jpg')
+# article = Article.new(title: 'NES', body: "A great console") sam-smith
+
+rowann_bush = User.new(
+  first_name: "Rowann",
+  last_name: "Bush",
+  password: "123456",
+  email: "rowannbush@gmail.com",
+  bio: "Run my own business from home making Xmas decorations",
+  availability: false
+)
+rowann_bush.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
+rowann_bush.save!
+
+file = URI.open('https://live.staticflickr.com/3800/10923845106_6192539e42_z.jpg')
+# article = Article.new(title: 'NES', body: "A great console") sam-smith
+
+
+kelly_britton = User.new(
+  first_name: "Kelly",
+  last_name: "Britton",
+  password: "123456",
+  email: "kellybritton@gmail.com",
+  bio: "Retired local copper I miss the beat, so always out on a stroll",
+  availability: false
+)
+kelly_britton.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
+kelly_britton.save!
+
+file = URI.open('https://digital-photography-school.com/wp-content/uploads/2008/07/portrait-background.jpg')
+# article = Article.new(title: 'NES', body: "A great console") sam-smith
+
+
+cleo_smart = User.new(
+  first_name: "Cleo",
+  last_name: "Smart",
+  password: "123456",
+  email: "cleosmart@gmail.com",
+  bio: "Retired maths teacher",
+  availability: false
+)
+cleo_smart.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
+cleo_smart.save!
+
+file = URI.open('https://live.staticflickr.com/7505/16155321812_98d2a992f8_b.jpg')
+# article = Article.new(title: 'NES', body: "A great console") sam-smith
+
+
+jessie_johnson = User.new(
+  first_name: "Jessie",
+  last_name: "Johnson",
+  password: "123456",
+  email: "jessiejohnson@gmail.com",
+  bio: "Brought up ten children, and lucky to have twenty five grandchildren",
+  availability: false
+)
+jessie_johnson.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
+jessie_johnson.save!
+
+file = URI.open('https://live.staticflickr.com/7454/13876667675_ab398a1c8e.jpg')
+# article = Article.new(title: 'NES', body: "A great console") sam-smith
+
+
+jackie_haruki = User.new(
+  first_name: "Jackie",
+  last_name: "Haruki",
+  password: "123456",
+  email: "jackieharuki@gmail.com",
+  bio: "Own the local sushi bar",
+  availability: false
+)
+jackie_haruki.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
+jackie_haruki.save!
