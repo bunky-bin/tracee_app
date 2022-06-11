48dc44c7981738456488e371a6502c0d212dc90d
Simone Piccolo <simonepiccolo1987@gmail.com>
2022-06-11 16:57:07 +0100

    created module and controller for livehelper

---
+++ test/controllers/livehelpers_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class LivehelpersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/livehelper_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class LivehelperTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
