c057871a6b5cb82942e08b94a9391330b92862a8
La-Velle <lavellesine@gmail.com>
2022-06-18 16:46:31 +0100

    metatags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 end
+
+# Rails image_url helper requires you setup your host to generate the absolute url needed
+# to load your images from the external world (Facebook, Twitter, ...).
+
+# Override Rails.application.default_url_options[:host]
+
+def default_url_options
+  { host: ENV["Tracee"] || "localhost:3000" }
+end
