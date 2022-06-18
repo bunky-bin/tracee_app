abc758d9ffd7ede636f69d0b105ac3f23c8a14f0
La-Velle <lavellesine@gmail.com>
2022-06-18 13:07:16 +0100

    demoday

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 end
+
+def default_url_options
+  { host: ENV["www.tracee808.org"] || "localhost:3000" }
+end
+
+# If you deploy your code with Heroku for instance, just type in your terminal:
+
+# heroku config:set DOMAIN=www.my_product.com
+
+# You can check it's properly set with heroku config:get DOMAIN.
