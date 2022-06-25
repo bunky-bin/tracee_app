2e383e04dfc805c8759e480ac74aed52bd5fc861
Simone Piccolo <simonepiccolo1987@gmail.com>
2022-06-25 17:12:52 +0100

    created mini popup for markers, cleared the extra page for new request

--- app/controllers/helpers_controller.rb
+++ app/controllers/helpers_controller.rb
@@ -8,16 +8,17 @@ class HelpersController < ApplicationController
     @markers = @users.geocoded.map do |user|
       {
         lat: user.latitude,
-        lng: user.longitude
-
-        # info_window: render_to_string(partial: "info_window", locals: { user: user })
+        lng: user.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { user: user })
       }
     end
   end
 
   def show
     @helper = User.find(params[:id])
-    # @request = Request.new
+     @request = Request.new()
     @reviews = @helper.reviews
+
+
   end
 end
--- app/javascript/controllers/mapboxIndex_controller.js
+++ app/javascript/controllers/mapboxIndex_controller.js
@@ -9,7 +9,7 @@ export default class extends Controller {
   }
 
   connect() {
-    console.log( "hallo")
+
     mapboxgl.accessToken = this.apiKeyValue
       this.map = new mapboxgl.Map({
       container: this.element,
@@ -17,20 +17,20 @@ export default class extends Controller {
     })
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
-    //this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken, mapboxgl: mapboxgl }))
+    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken, mapboxgl: mapboxgl }))
   }
 
   #addMarkersToMap() {
-    console.log(this.markersValue)
     this.markersValue.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window)
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup)
         .addTo(this.map)
-
     });
   }
+
+
 
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
