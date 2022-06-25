94ef91d7aaf980948e4957a26e1e0c5c08edc62b
La-Velle <lavellesine@gmail.com>
2022-06-25 16:56:48 +0100

    stars

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,11 @@ class Review < ApplicationRecord
   belongs_to :request
   belongs_to :helper, class_name: "User", optional: true
   validates :rating, numericality: true
-  validates :description, length: { minimum: 5 }
+  validates :description, length: { minimum: 2 }
 
 end
+
+# ADVISED MODEL NEEDS :
+# content:string
+# rating:integer
+# user:references
